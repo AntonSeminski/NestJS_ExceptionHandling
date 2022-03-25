2d9d4ed7ddfe6fe03fb1d9db03820effbc353ad1
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2022-03-25 16:05:59 +0300

    v. 1.0.9 CustomException body fix

--- src/handlers/custom/custom-exception.handler.ts
+++ src/handlers/custom/custom-exception.handler.ts
@@ -1,19 +1,12 @@
 import {ExceptionHandler} from "../abstract/exception.handler";
 import {HandledExceptionDto} from "../../dto/handled-exception.dto";
 import {CustomException} from "../../exceptions/custom.exception";
-import {CODES} from "../../constants/codes.constants";
-
 
 export class CustomExceptionHandler extends ExceptionHandler {
     handle(exception: any): HandledExceptionDto {
         if ( !(exception instanceof CustomException) )
             return this.next?.handle(exception)
 
-        const code = exception.code ? exception.code : CODES.COMMON.HTTP;
-        const status = exception.getStatus();
-        const message= exception.message;
-
-        return new HandledExceptionDto({code, status, message});
+        return new HandledExceptionDto(exception);
     }
-
 }
